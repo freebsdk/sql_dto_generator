84f0c5e8bab0cd1a038ce6752b824bf28ace4da6
SeokYoung Shin <aceorb@gmail.com>
2023-12-07 11:33:24 +0900

    IscriptGenerator 정리

--- sqlc/ScriptGenerator/IScriptGenerator.cs
+++ sqlc/ScriptGenerator/IScriptGenerator.cs
@@ -1,9 +1,23 @@
+using System.Text;
+
 namespace sqlc;
 
 public abstract class IScriptGenerator
 {
+    protected readonly Dictionary<IdlFieldType, string /* native type */> _nativeTypes = new();
+    protected readonly StringBuilder _genCode = new();
+    
+    
+    
     public abstract void Open();
     public abstract void Close();
     public abstract void GenerateTable(TableInfo tableInfo);
-    public abstract string GetGeneratedCode();
+
+
+
+
+    public string GetGeneratedCode()
+    {
+        return _genCode.ToString();
+    }
 }
